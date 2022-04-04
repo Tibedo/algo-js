c31f7a3a1bb269d6463b426f30ca686b2b1baf45
Tibedo <thibaudbaily@hotmail.com>
2022-04-04 15:42:29 +0200

    Ex 1.0

---
+++ 1.0.js
@@ -0,0 +1,35 @@
+
+// Addition
+let a = 4;
+let b = 5;
+let add = a + b;
+
+console.log(add);
+
+// Soustraction
+let a = 4;
+let b = 5;
+let sous = a - b;
+
+console.log(sous);
+
+// Multiplication
+let a = 4;
+let b = 5;
+let mult = a * b;
+
+console.log(mult);
+
+// Division
+let a = 4;
+let b = 5;
+let div = a / b;
+
+console.log(div);
+
+// Modulo
+let a = 4;
+let b = 5;
+let mod = a % b;
+
+console.log(mod);
