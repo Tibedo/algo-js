b00d0da00460370059d9bd9eda927cd1f4c6c871
Tibedo <thibaudbaily@hotmail.com>
2022-04-05 16:46:04 +0200

    2.3.js

---
+++ 2.2.js
@@ -0,0 +1,20 @@
+
+let min = prompt("Entrez un nombre minimal")
+let max = prompt("Entrez un nombre maximal")
+let current
+
+
+if (min > max && max < min)  {
+    alert("noob") ;
+    
+}
+
+current=prompt("Entrez un nombre entre la valeur min et max")
+
+if (current > min && current < max) {
+    alert("good")
+}
+
+else {
+    alert("You failed noob")
+}
---
+++ 2.3.js
@@ -0,0 +1,5 @@
+let num = prompt("Entrez un nombre compris entre 1 et 100")
+
+for (i=1; i <= 50; i++) {
+    alert(i)
+}
